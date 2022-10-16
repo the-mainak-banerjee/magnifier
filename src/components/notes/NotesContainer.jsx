3a827548557844bcc4d0422d1c73c41ceb6b3daa
Mainak Banerjee <developer.mainakbanerjee@gmail.com>
2022-10-16 19:51:59 +0530

    fixed some issue

--- src/components/notes/NotesContainer.jsx
+++ src/components/notes/NotesContainer.jsx
@@ -5,12 +5,13 @@ import { NotesItem } from './NotesItem'
 
 export const NotesContainer = ({notes}) => {
 
-    const { pinnedNotes } = useNotes()
+    const { pinnedNotes, onTrashPage, onArchivePage } = useNotes()
+
 
 
   return (
-    <Container maxW="container.lg" p='0'>
-        {(pinnedNotes?.length > 0 && notes?.length > 0) && <Text fontSize='md' fontWeight='semiBold' ml='6'>OTHERS</Text>}
+    <Container maxW="container.lg" p='0' my={(!onTrashPage && !onArchivePage) ? '0' : '10'}>
+        {(pinnedNotes?.length > 0 && notes?.length > 0 && !onTrashPage && !onArchivePage) && <Text fontSize='md' fontWeight='semiBold' ml='6'>OTHERS</Text>}
         <Flex flexWrap='wrap'>
             {notes?.map(note => {
                 return (
